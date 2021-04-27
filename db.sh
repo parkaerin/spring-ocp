c42b39e0632189888b0d8df2b6e9f605bd1874e2
root <root@localhost.localdomain>
2021-04-27 11:08:21 -0400

    changed some files

--- db.sh
+++ db.sh
@@ -3,9 +3,10 @@
 docker rm -f demodb
 
 docker run -d --name demodb \
+--network demo \
 -p 30306:3306 \
 -e MYSQL_USER=testuser \
--e MYSQL_ROOT_PASSWORD=pass \
+-e MYSQL_ROOT_PASSWORD=rootpass \
 -e MYSQL_DATABASE=testdb \
 -e MYSQL_PASSWORD=testpass \
 -v ~/spring-ocp/sql/:/docker-entrypoint-initdb.d/ \
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm -f demoapp
-docker run -d -p 8080:8080 --name demoapp demoapp
+docker run --network demo -d -e MYSQL_USER=testuser -e MYSQL_PASSWORD=testpass -p 8080:8080 --name demoapp demoapp
